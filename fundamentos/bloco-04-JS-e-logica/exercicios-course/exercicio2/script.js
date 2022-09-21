c649be8a317c52424fd96a7dc6e4d87550a9a2b2
Vinícius Melo <viniciusmelo317@gmail.com>
2022-09-21 11:32:00 -0300

    resolução exercício 2 do course

---
+++ fundamentos/bloco-04-JS-e-logica/exercicios-course/exercicio2/script.js
@@ -0,0 +1,8 @@
+const base = 5;
+let height = 8;
+
+const area = base * height;
+console.log('A área do retângulo é: ' + area);
+
+const perimeter = (base * 2) + (height * 2);
+console.log('O perímetro do retângulo é: ' + perimeter);
