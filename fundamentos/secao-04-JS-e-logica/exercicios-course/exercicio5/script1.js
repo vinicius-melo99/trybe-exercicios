d021b8340cc735ad2626db0b7097850a958d074b
Vinícius Melo <viniciusmelo317@gmail.com>
2022-09-22 09:38:37 -0300

    resulução 1º exercicio da aba 'exercicios'

---
+++ fundamentos/secao-04-JS-e-logica/exercicios-course/exercicio5/script1.js
@@ -0,0 +1,17 @@
+const a = 10, b = 2;
+let soma, sub, multi, divisao, mod;
+
+soma = a + b;
+console.log(`O resultado de ${a} + ${b} = ${soma}`);
+
+sub = a - b;
+console.log(`O resultado de ${a} - ${b} = ${sub}`);
+
+multi = a * b;
+console.log(`O resultado de ${a} x ${b} = ${multi}`);
+
+divisao = a / b;
+console.log(`O resultado de ${a} / ${b} = ${divisao}`);
+
+mod = a % b;
+console.log(`O resultado de ${a} % ${b} = ${mod}`);
