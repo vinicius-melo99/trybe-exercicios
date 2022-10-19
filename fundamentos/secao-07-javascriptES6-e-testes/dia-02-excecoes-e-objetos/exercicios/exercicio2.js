0678e366e6dcfb038ff7c51e3e54150c3ed1f095
Vinícius Melo <viniciusmelo317@gmail.com>
2022-10-19 11:17:14 -0300

    completado exericio 2 do course

---
+++ fundamentos/secao-07-javascriptES6-e-testes/dia-02-excecoes-e-objetos/exercicios/exercicio2.js
@@ -0,0 +1,54 @@
+//exercício 2 - pedidos de clientes
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        },
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        },
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      },
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
+    return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, N°: ${order.address.number}, AP: ${order.address.apartment}`;
+  };
+  
+  console.log(customerInfo(order));
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+    order.name = 'Luiz Silva';
+    order.payment = 50;
+
+    return `Olá ${order.name}, o total do seu pedido de marguerita, pepperoni e ${order.order.drinks.coke.type} é R$ ${order.payment}`;
+  
+  };
+  
+  console.log(orderModifier(order));
